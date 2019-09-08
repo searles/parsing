89fd326e3b05eac633705eff762e9d0a96b840cd
searles <karl.gmeiner@gmail.com>
2019-09-08 21:58:18 +0200

    Add API to LookAheadException

--- src/main/java/at/searles/parsing/ParserLookaheadException.java
+++ src/main/java/at/searles/parsing/ParserLookaheadException.java
@@ -3,10 +3,39 @@ package at.searles.parsing;
 public class ParserLookaheadException extends RuntimeException {
 
     private final Recognizable.Then failedParser;
-    private final ParserStream stream;
+    private final ParserStream source;
+    private final long start;
+    private final long end;
 
     public ParserLookaheadException(Recognizable.Then failedParser, ParserStream stream) {
-        this.stream = stream;
+        this.source = stream;
+        this.start = stream.start();
+        this.end = stream.end();
+
         this.failedParser = failedParser;
+    }
+
+    // TODO unit test!
+
+    /**
+     * @return The position of the successful match before the mismatch
+     */
+    public long start() {
+        return start;
+    }
+
+    /**
+     * @return The end position of the successful match before the mismatch
+     */
+    public long end() {
+        return end;
+    }
+
+    public Recognizable.Then failedParser() {
+        return failedParser;
+    }
+
+    public ParserStream source() {
+        return source;
     }
 }
