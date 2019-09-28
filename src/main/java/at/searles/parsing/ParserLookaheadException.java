999d21fcbeeb146654f6fcb5263ed55408d7c4ea
searles <karl.gmeiner@gmail.com>
2019-09-29 01:10:49 +0200

    Improve LA Exception
    * Also remove unused classes.

--- src/main/java/at/searles/parsing/ParserLookaheadException.java
+++ src/main/java/at/searles/parsing/ParserLookaheadException.java
@@ -4,31 +4,31 @@ public class ParserLookaheadException extends RuntimeException {
 
     private final Recognizable.Then failedParser;
     private final ParserStream source;
-    private final long start;
-    private final long end;
+
+    private final long beforeStart;
+    private final long beforeEnd;
+    private long nextTokenStart;
+    private long nextTokenEnd;
 
     public ParserLookaheadException(Recognizable.Then failedParser, ParserStream stream) {
         this.source = stream;
-        this.start = stream.start();
-        this.end = stream.end();
+
+        this.beforeStart = stream.start();
+        this.beforeEnd = stream.end();
+
+        this.nextTokenStart = stream.tokStream().offset();
+        this.nextTokenEnd = stream.tokStream().frame().endPosition();
+
+        if(nextTokenStart < beforeEnd) {
+            // this would surprise me...
+            nextTokenStart = beforeEnd;
+        }
+
+        if(nextTokenEnd <= nextTokenStart) {
+            nextTokenEnd = nextTokenStart + 1;
+        }
 
         this.failedParser = failedParser;
-    }
-
-    // TODO unit test!
-
-    /**
-     * @return The position of the successful match before the mismatch
-     */
-    public long start() {
-        return start;
-    }
-
-    /**
-     * @return The end position of the successful match before the mismatch
-     */
-    public long end() {
-        return end;
     }
 
     public Recognizable.Then failedParser() {
@@ -37,5 +37,25 @@ public class ParserLookaheadException extends RuntimeException {
 
     public ParserStream source() {
         return source;
+    }
+
+    public long getBeforeStart() {
+        return beforeStart;
+    }
+
+    public long getBeforeEnd() {
+        return beforeEnd;
+    }
+
+    public long getNextTokenStart() {
+        return nextTokenStart;
+    }
+
+    public long getNextTokenEnd() {
+        return nextTokenEnd;
+    }
+
+    public String toString() {
+        return String.format("%s expected after %s at %d-%d", failedParser.right(), failedParser.left(), getNextTokenStart(), getNextTokenEnd());
     }
 }
