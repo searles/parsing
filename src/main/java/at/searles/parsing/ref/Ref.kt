728b34b2d2b1f0cd697e67a601dd48eae8095035
searles <karl.gmeiner@gmail.com>
2020-09-18 22:01:55 +0200

    Add error message for uninitialized Ref

--- src/main/java/at/searles/parsing/ref/Ref.kt
+++ src/main/java/at/searles/parsing/ref/Ref.kt
@@ -6,9 +6,18 @@ import at.searles.parsing.printing.ConcreteSyntaxTree
 
 class Ref<T>(private val label: String) : Parser<T> {
 
-    lateinit var ref: Parser<T>
+    var ref: Parser<T>
+        get() = internalRef ?: error("$label is not initialized")
+
+        set(value) {
+            internalRef = value
+        }
+
+    private var internalRef: Parser<T>? = null
 
     override fun parse(stream: ParserStream): T? {
+
+
         stream.fireRefStart(label)
         return ref.parse(stream).also {
             if(it != null) {
